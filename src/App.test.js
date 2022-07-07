ced01d197b4b09b18a943fadba55b279f73d7686
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-07-07 16:04:23 +0530

    [SURAJ] Added : TDD React

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it("shouldReturnHelloWorld", ()=>{
+  const component = render(<App/>);
+  const componentValue=component.getByTestId("1");
+  expect(componentValue).toHaveTextContent("Hello World")
+
+})
